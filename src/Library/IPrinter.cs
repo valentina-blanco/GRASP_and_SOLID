8931f20f8762964971147269c54edb4a64ba2f00
belenkanas <maria.kanas@correo.ucu.edu.uy>
2024-10-24 08:56:47 -0300

    PrintRecipe e implementacion

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -3,10 +3,11 @@ namespace Full_GRASP_And_SOLID
 {
     public interface IPrinter
     {
-        /// <summary>
+         /// <summary>
         /// Modificamos el parámetro para que dependa de la clase abstracta PrintRecipe y no sólo de la clase Recipe
         /// </summary>
         /// <param name="recipe"></param>
          void PrintRecipe(PrintRecipe recipe);
+         
     }
 }
